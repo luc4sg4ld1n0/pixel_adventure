81975c4071f37e8277fdede99e5c92121645d979
Lucas Galdino <l0b0br4nc0@proton.me>
2025-10-07 14:26:13 -0300

    testing

---
+++ lib/pixel_adventure.dart
@@ -0,0 +1,78 @@
+import 'dart:async';
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/widgets.dart';
+import '/actors/player.dart';
+import '/levels/level.dart';
+
+class PixelAdventure extends FlameGame
+    with HasKeyboardHandlerComponents, DragCallbacks {
+  @override
+  Color backgroundColor() => const Color(0xFF211F30);
+  late final CameraComponent cam;
+  Player player = Player();
+  late JoystickComponent joystick;
+  // controles mobile(true) e desktop(false)
+  bool showJoystick = false;
+
+  @override
+  FutureOr<void> onLoad() async {
+    await images.loadAllImages();
+
+    final world = Level(player: player, levelName: 'Level-01');
+
+    cam = CameraComponent.withFixedResolution(
+      world: world,
+      width: 640,
+      height: 360,
+    );
+    cam.viewfinder.anchor = Anchor.topLeft;
+
+    addAll([cam, world]);
+
+    if (showJoystick) {
+      addJoystick();
+    }
+
+    return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    if (showJoystick) {
+      updateJoystick();
+    }
+    super.update(dt);
+  }
+
+  void addJoystick() {
+    joystick = JoystickComponent(
+      knob: SpriteComponent(sprite: Sprite(images.fromCache('HUD/Knob.png'))),
+      background: SpriteComponent(
+        sprite: Sprite(images.fromCache('HUD/Joystick.png')),
+      ),
+      margin: const EdgeInsets.only(left: 32, bottom: 32),
+    );
+
+    add(joystick);
+  }
+
+  void updateJoystick() {
+    switch (joystick.direction) {
+      case JoystickDirection.left:
+      case JoystickDirection.upLeft:
+      case JoystickDirection.downLeft:
+        player.playerDirection = PlayerDirection.left;
+        break;
+      case JoystickDirection.right:
+      case JoystickDirection.upRight:
+      case JoystickDirection.downRight:
+        player.playerDirection = PlayerDirection.right;
+        break;
+      default:
+        player.playerDirection = PlayerDirection.none;
+        break;
+    }
+  }
+}
